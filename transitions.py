c7615fff94a749790e14e3dbfdfcf72a66d5e145
Unknown <grama.dragos.ioan@gmail.com>
2019-04-06 17:52:29 +0200

    Changed a rule

--- transitions.py
+++ transitions.py
@@ -32,19 +32,27 @@ def valid_transition(state1,state2):
             return 0
    
         
-    # exclude transitions that change the derivative without changing the value except the inflow which follows a parabola
-    if state1[0] == state2[0] and state1[1] == state2[1]:
+    # exclude transitions that change the derivative without changing the value when the inflow is the same 
+    # or the magnitude or the derivative of the inflow is 0 
+    if state1[0] == state2[0] and state1[1] == state2[1] or state1[0] == 0 or state1[1] == 0:
         for i in range(1,int(len(state1)/2)):
-            if state1[2*i] == state2[2*i] and state1[2*i+1] != state2[2*i+1]:
-                return 0
-            
-    # exclude transitions that change the derivative of the other quantities when the inflow is static and the other quantities are not         
-    if state1[0] == 1 and state1[1] == 0 or state1[0] ==0 and state1[1] == 1:
-        for i in range(1,int(len(state1)/2)):
-            if state1[2*i+1] !=0 and state1[2*i+1] != state2[2*i+1]:
-                return 0
+            if state1[2*i] == state2[2*i] and state1[2*i+1] != state2[2*i+1] and state1[2*i+1] != 0:
+                return 0            
+        return 0   
          
     return 1
+
+def transition_states(state,validS):
+    result = []
+    for i in range(np.shape(validS)[0]):
+        if valid_transition(state,validS[i,:]) == 1:
+            result.append(validS[i,:])
+    
+    return np.array(result)
+
+
+aux = transition_states(aux2[0],validS)
+    
             
             
 
